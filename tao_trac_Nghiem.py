13b89f4e3e95f3ec03815fbc9afe6e96122a9143
minhlongbeo <voleminhlong2358@gmail.com>
2024-11-25 11:13:34 +0700

    Update tao_trac_Nghiem.py 1.3 rebug

--- tao_trac_Nghiem.py
+++ tao_trac_Nghiem.py
@@ -11,7 +11,7 @@ mang_du_lieu = nr.array([
     },
 
     {
-        'question3' : '5 + 7 = ?',  'awer1' : '3',  'awer2' : '6',  'awer3' : '8',  'awer4' : '12',  'answer' : '4'
+        'question3' : '5 + 7 = ?',  'awer1' : '3',  'awer2' : '65646755406',  'awer3' : '8',  'awer4' : '12',  'answer' : '4'
     },
 
     {
@@ -48,7 +48,10 @@ mang_du_lieu = nr.array([
 class system_code:
     def __init__(self, mang, thoi_gian, don_vi, kieu, kick_thuoc):
 
-        bien_x_dap_an = 6
+        bien_x_cau_hoi = kick_thuoc/2.6
+        khoang_cach_x = 145
+        bien_x_dap_an = 60
+        bien_y_them = 100
         bien_y = 20
         dem = 0
 
@@ -56,36 +59,62 @@ class system_code:
         giao_dien.title("trắc nghiệm")
         giao_dien.geometry(f'{kick_thuoc}x{kick_thuoc}')
 
+        #0.0.1---------------------------
+        def kiem_tra_text(textt):
+            textz = ''
+            st = 0
+
+            for e in textt:
+                if st!=kick_thuoc:       textz+=e
+                elif st==kick_thuoc:    textz+=(e + '\n');   st=0
+
+                st+=1
+                
+            return textz
+
+        #0.10.1-------------------------=
+
         for vi_tri, widget_nho in enumerate(mang):
 
             chu_cau_hoi = widget_nho[ 'question' + str(vi_tri+1) ]
 
-            #kiểm tra 1
-            if len(chu_cau_hoi) > 10:
-                textz = ''
-                st = 0
+            #tạo label
+            cau_hoi = Label(giao_dien,  text=chu_cau_hoi,  font = kieu)
+            thu_tu = Label(giao_dien,  text=f'{vi_tri+1} |',  font=kieu)
+            dap_an_bien = nr.array([
+            Button(giao_dien,  text=widget_nho['awer1'],  font=kieu),
+            Button(giao_dien,  text=widget_nho['awer2'],  font=kieu),
+            Button(giao_dien,  text=widget_nho['awer3'],  font=kieu),
+            Button(giao_dien,  text=widget_nho['awer4'],  font=kieu)
+            ], dtype=object)
 
-                #xử lí text
-                for e in chu_cau_hoi:
-                    if st!=kick_thuoc:       textz+=e
-                    elif st==kick_thuoc:    textz+=(e + '\n');   st=0
+            #place
+            thu_tu.place(x = bien_x_cau_hoi,  y = bien_y)
+            thu_tu.update()
+            cau_hoi.place(x = (thu_tu.winfo_x() + 40), y = bien_y)
 
-                    st+=1
-                
-                chu_cau_hoi = textz
+            index_y = bien_y + 50
 
+            dap_an_bien[0].place(x = bien_x_dap_an,  y = index_y)
+            dap_an_bien[1].place(x = bien_x_dap_an + khoang_cach_x,  y = index_y)
+            dap_an_bien[2].place(x = bien_x_dap_an + khoang_cach_x * 2,  y = index_y)
+            dap_an_bien[3].place(x = bien_x_dap_an + khoang_cach_x * 3,  y = index_y)
 
-            #kiểm tra 1
+            bien_y += 125
 
-            cau_hoi = Label(giao_dien,  text=chu_cau_hoi, font = kieu)
+            giao_dien.update()
 
-            cau_hoi.place(x = 6, y = bien_y)
+            #kiểm tra độ dài
+            for v in range(0, len(dap_an_bien)-1):
+                widget_mini = dap_an_bien[v]
 
-            bien_y += 100
+                if widget_mini.winfo_x() + widget_mini.winfo_width() >= dap_an_bien[v+1].winfo_x():
+                    widget_mini.config(text='...')
+                    widget_mini.config(command=lambda)
+            #----
 
 
         giao_dien.mainloop()
-
 
 
 system_code(mang_du_lieu, 0, 'giây', ('Times New Roman', 20), 600)
