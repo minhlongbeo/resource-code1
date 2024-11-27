f8e40de866ee0305b3032f1543add5ef0c9149bf
minhlongbeo <voleminhlong2358@gmail.com>
2024-11-27 10:35:35 +0700

    Update tao_trac_Nghiem.py 1.4 debug and recode
    for some reason this app can be realease but late =((

--- tao_trac_Nghiem.py
+++ tao_trac_Nghiem.py
@@ -1,5 +1,6 @@
 import numpy as nr
 from tkinter import *
+from text_config import *
 
 mang_du_lieu = nr.array([ 
     {
@@ -44,79 +45,94 @@ mang_du_lieu = nr.array([
 
     ], dtype = dict )
 
+def tao_widget(giao_dien,  kick_thuoc,  widget_nho,  kieu,  vi_tri,  x1,  x2,  x3, y_s, y_x):
+    chu_cau_hoi = widget_nho[ 'question' + str(vi_tri+1) ]
+
+    #tạo label
+    dap_an_bien = nr.array([
+    Label(giao_dien,  text=f'{vi_tri+1} |',  font=kieu),
+    Label(giao_dien,  text=chu_cau_hoi,  font = kieu),
+    Button(giao_dien,  text=widget_nho['awer1'],  font=kieu),
+    Button(giao_dien,  text=widget_nho['awer2'],  font=kieu),
+    Button(giao_dien,  text=widget_nho['awer3'],  font=kieu),
+     Button(giao_dien,  text=widget_nho['awer4'],  font=kieu)
+    ], dtype=object)
+
+    #place
+    dap_an_bien[0].place(x = x1, y = y_x)
+    dap_an_bien[1].place(x = x1 + 40 , y = y_x)
+
+    index_y = y_x + 50
+
+    dap_an_bien[2].place(x = x2,  y = index_y)
+    dap_an_bien[3].place(x = x2 +x3,  y = index_y)
+    dap_an_bien[4].place(x = x2 + x3 * 2,  y = index_y)
+    dap_an_bien[5].place(x = x2 + x3 * 3,  y = index_y)
+
+    giao_dien.update()
+
+    #tạo gui hiển thị nhỏ
+    def tao_gui_hien_thi(textz):
+        gui_new = Tk()
+        gui_new.title(f'câu hỏi thứ {vi_tri}')
+        chu = Label(gui_new,  text=textz,  font=kieu)
+        chu.pack()
+        chu.update()
+        gui_new.geometry(f'{chu.winfo_width() + 70}x{chu.winfo_height() + 20}')
+
+    #kiểm tra độ dài
+    for v in range(0, len(dap_an_bien)-1):
+        mini = dap_an_bien[v]
+        x_mini = mini.winfo_x()
+        y_mini = mini.winfo_y()
+        with_mini = mini.winfo_width()
+        height_mini = mini.winfo_height()
+
+        if v >= 2 and x_mini + with_mini >= dap_an_bien[v+1].winfo_x():
+            chu_luu = mini.cget('text')
+            mini.config(text='...')
+            mini.config(command=lambda : tao_gui_hien_thi(chu_luu) )
+
+        elif y_mini + height_mini >= kick_thuoc:
+            #kéo thêm dòng nếu gần
+            if y_mini + height_mini - y_s < kick_thuoc:
+                giao_dien.geometry(f'{kick_thuoc}x{kick_thuoc + y_s}')
+                return True
+                
+    return False
+#------------------------------------------------------------------------------------------------------
 
 class system_code:
     def __init__(self, mang, thoi_gian, don_vi, kieu, kick_thuoc):
-
+        do_khoang_cach_y = int(kick_thuoc/8.3)
         bien_x_cau_hoi = kick_thuoc/2.6
+        chuyen_trang_old = False
+        chua_gui = nr.array([], dtype=object)
         khoang_cach_x = 145
         bien_x_dap_an = 60
-        bien_y_them = 100
         bien_y = 20
-        dem = 0
+        i = 1
 
         giao_dien = Tk()
-        giao_dien.title("trắc nghiệm")
+        giao_dien.title(f"trang trắc nghiệm {i}")
         giao_dien.geometry(f'{kick_thuoc}x{kick_thuoc}')
 
-        #0.0.1---------------------------
-        def kiem_tra_text(textt):
-            textz = ''
-            st = 0
-
-            for e in textt:
-                if st!=kick_thuoc:       textz+=e
-                elif st==kick_thuoc:    textz+=(e + '\n');   st=0
-
-                st+=1
-                
-            return textz
-
-        #0.10.1-------------------------=
-
-        for vi_tri, widget_nho in enumerate(mang):
-
-            chu_cau_hoi = widget_nho[ 'question' + str(vi_tri+1) ]
-
-            #tạo label
-            cau_hoi = Label(giao_dien,  text=chu_cau_hoi,  font = kieu)
-            thu_tu = Label(giao_dien,  text=f'{vi_tri+1} |',  font=kieu)
-            dap_an_bien = nr.array([
-            Button(giao_dien,  text=widget_nho['awer1'],  font=kieu),
-            Button(giao_dien,  text=widget_nho['awer2'],  font=kieu),
-            Button(giao_dien,  text=widget_nho['awer3'],  font=kieu),
-            Button(giao_dien,  text=widget_nho['awer4'],  font=kieu)
-            ], dtype=object)
-
-            #place
-            thu_tu.place(x = bien_x_cau_hoi,  y = bien_y)
-            thu_tu.update()
-            cau_hoi.place(x = (thu_tu.winfo_x() + 40), y = bien_y)
-
-            index_y = bien_y + 50
-
-            dap_an_bien[0].place(x = bien_x_dap_an,  y = index_y)
-            dap_an_bien[1].place(x = bien_x_dap_an + khoang_cach_x,  y = index_y)
-            dap_an_bien[2].place(x = bien_x_dap_an + khoang_cach_x * 2,  y = index_y)
-            dap_an_bien[3].place(x = bien_x_dap_an + khoang_cach_x * 3,  y = index_y)
+        #lặp tạo câu hỏi 
+        for vi_tri, phan_con in enumerate(mang):
+            chuyen_trang = tao_widget(giao_dien,  kick_thuoc,  phan_con,  kieu,  vi_tri,  bien_x_cau_hoi,   bien_x_dap_an,  khoang_cach_x,  do_khoang_cach_y,  bien_y)
 
             bien_y += 125
 
-            giao_dien.update()
+            if chuyen_trang == True and vi_tri + 1 < len(mang):
+                chua_gui = nr.append(chua_gui, giao_dien)
 
-            #kiểm tra độ dài
-            for v in range(0, len(dap_an_bien)-1):
-                widget_mini = dap_an_bien[v]
+                giao_dien = Tk()
+                giao_dien.title(f'trang trắc nghiệm {i+1}')
+                giao_dien.geometry(f'{kick_thuoc}x{kick_thuoc}')
 
-                if widget_mini.winfo_x() + widget_mini.winfo_width() >= dap_an_bien[v+1].winfo_x():
-                    widget_mini.config(text='...')
-                    widget_mini.config(command=lambda)
-            #----
-
+                bien_y = 20
 
         giao_dien.mainloop()
-
-
 system_code(mang_du_lieu, 0, 'giây', ('Times New Roman', 20), 600)
 
 
