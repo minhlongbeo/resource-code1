bbe3e2c3e42bd430f8436bcf8e43cf3e770137db
minhlongbeo <voleminhlong2358@gmail.com>
2024-11-29 11:09:31 +0700

    Update tao_trac_Nghiem.py 1.0

--- tao_trac_Nghiem.py
+++ tao_trac_Nghiem.py
@@ -1,6 +1,7 @@
-import numpy as nr
+from module_mini import *
+
 from tkinter import *
-from text_config import *
+import numpy as nr
 
 mang_du_lieu = nr.array([ 
     {
@@ -45,7 +46,7 @@ mang_du_lieu = nr.array([
 
     ], dtype = dict )
 
-def tao_widget(giao_dien,  kick_thuoc,  widget_nho,  kieu,  vi_tri,  x1,  x2,  x3, y_s, y_x):
+def tao_widget(giao_dien,  kick_thuoc,  widget_nho,  kieu,  vi_tri,  x_y):
     chu_cau_hoi = widget_nho[ 'question' + str(vi_tri+1) ]
 
     #tạo label
@@ -59,26 +60,17 @@ def tao_widget(giao_dien,  kick_thuoc,  widget_nho,  kieu,  vi_tri,  x1,  x2,  x
     ], dtype=object)
 
     #place
-    dap_an_bien[0].place(x = x1, y = y_x)
-    dap_an_bien[1].place(x = x1 + 40 , y = y_x)
+    dap_an_bien[0].place(x = x_y[0], y = x_y[-1])
+    dap_an_bien[1].place(x = x_y[0] + 40 , y = x_y[-1])
 
-    index_y = y_x + 50
+    index_y = x_y[-1] + 50
 
-    dap_an_bien[2].place(x = x2,  y = index_y)
-    dap_an_bien[3].place(x = x2 +x3,  y = index_y)
-    dap_an_bien[4].place(x = x2 + x3 * 2,  y = index_y)
-    dap_an_bien[5].place(x = x2 + x3 * 3,  y = index_y)
+    dap_an_bien[2].place(x = x_y[1],  y = index_y)
+    dap_an_bien[3].place(x = x_y[1] + x_y[2],  y = index_y)
+    dap_an_bien[4].place(x = x_y[1] + x_y[2] * 2,  y = index_y)
+    dap_an_bien[5].place(x = x_y[1] + x_y[2] * 3,  y = index_y)
 
     giao_dien.update()
-
-    #tạo gui hiển thị nhỏ
-    def tao_gui_hien_thi(textz):
-        gui_new = Tk()
-        gui_new.title(f'câu hỏi thứ {vi_tri}')
-        chu = Label(gui_new,  text=textz,  font=kieu)
-        chu.pack()
-        chu.update()
-        gui_new.geometry(f'{chu.winfo_width() + 70}x{chu.winfo_height() + 20}')
 
     #kiểm tra độ dài
     for v in range(0, len(dap_an_bien)-1):
@@ -91,27 +83,29 @@ def tao_widget(giao_dien,  kick_thuoc,  widget_nho,  kieu,  vi_tri,  x1,  x2,  x
         if v >= 2 and x_mini + with_mini >= dap_an_bien[v+1].winfo_x():
             chu_luu = mini.cget('text')
             mini.config(text='...')
-            mini.config(command=lambda : tao_gui_hien_thi(chu_luu) )
+            mini.config(command=lambda : mo_to_gui(chu_luu, vi_tri) )
 
         elif y_mini + height_mini >= kick_thuoc:
             #kéo thêm dòng nếu gần
-            if y_mini + height_mini - y_s < kick_thuoc:
-                giao_dien.geometry(f'{kick_thuoc}x{kick_thuoc + y_s}')
-                return True
+            
+            if y_mini + height_mini - x_y[-2] < kick_thuoc:                
+                giao_dien.geometry(f'{kick_thuoc}x{kick_thuoc + x_y[-2]+20}')
                 
+                print(f'widget là 2{widget_nho}')
+                return True
+            
     return False
 #------------------------------------------------------------------------------------------------------
 
 class system_code:
     def __init__(self, mang, thoi_gian, don_vi, kieu, kick_thuoc):
-        do_khoang_cach_y = int(kick_thuoc/8.3)
+        
+        do_khoang_cach_y = int(kick_thuoc/8.1)
+        chua_gui = []
         bien_x_cau_hoi = kick_thuoc/2.6
         chuyen_trang_old = False
-        chua_gui = nr.array([], dtype=object)
-        khoang_cach_x = 145
-        bien_x_dap_an = 60
-        bien_y = 20
-        i = 1
+        bien_y = 6
+        i = 0
 
         giao_dien = Tk()
         giao_dien.title(f"trang trắc nghiệm {i}")
@@ -119,20 +113,48 @@ class system_code:
 
         #lặp tạo câu hỏi 
         for vi_tri, phan_con in enumerate(mang):
-            chuyen_trang = tao_widget(giao_dien,  kick_thuoc,  phan_con,  kieu,  vi_tri,  bien_x_cau_hoi,   bien_x_dap_an,  khoang_cach_x,  do_khoang_cach_y,  bien_y)
+            chuyen_trang = tao_widget(giao_dien,  kick_thuoc,  phan_con,  kieu,  vi_tri,  [bien_x_cau_hoi, 60,  145,  do_khoang_cach_y,  bien_y])
 
             bien_y += 125
+            
+            if chuyen_trang == True:
+                print(f'yessssss {vi_tri} và {len(mang)}')
 
-            if chuyen_trang == True and vi_tri + 1 < len(mang):
-                chua_gui = nr.append(chua_gui, giao_dien)
+            vi_tri_set = vi_tri + 1 < len(mang)
+
+            if chuyen_trang == True and vi_tri_set == True:
+                
+                chua_gui.append(giao_dien)
+                anh_undo = PhotoImage(file='C:/Users/Minh Long/AppData/Roaming/Txt_file/Icon/view_undo.png')
+
+                anh_next = PhotoImage(file='C:/Users/Minh Long/AppData/Roaming/Txt_file/Icon/view_more.png')
+                nut_next = Button(giao_dien, image=anh_next)
+                nut_next.place(x = kick_thuoc/1.75,  y = bien_y)
+
+                nut_undo = Button(giao_dien,   image=anh_undo)
+                nut_undo.place(x = kick_thuoc/3,  y = bien_y)
 
                 giao_dien = Tk()
-                giao_dien.title(f'trang trắc nghiệm {i+1}')
+                giao_dien.title(f'trang trắc nghiệm {i+2}')
                 giao_dien.geometry(f'{kick_thuoc}x{kick_thuoc}')
+                giao_dien.withdraw()
 
+                def chuyen_trang(linh_tinh):
+                    chua_gui[i].withdraw()
+                    giao_dien.deiconify()
+                
+                nut_next.bind('<Button>',  chuyen_trang)
+                
                 bien_y = 20
+        #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+        #chuyển trang
         giao_dien.mainloop()
+        
 system_code(mang_du_lieu, 0, 'giây', ('Times New Roman', 20), 600)
+        
+system_code(mang_du_lieu, 0, 'giây', ('Times New Roman', 20), 600)
+
+
 
 
