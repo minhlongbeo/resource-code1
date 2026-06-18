ce733ad35ad0da3df5363c9fa0fed47c3519cc2c
minhlongbeo <voleminhlong2358@gmail.com>
2026-06-18 15:33:36 +0700

    Update tao_trac_Nghiem.py

--- tao_trac_Nghiem.py
+++ tao_trac_Nghiem.py
@@ -25,14 +25,14 @@
         <button type="button" onclick="CheckNumber(4)">4</button>
         <button type="button" onclick="CheckNumber(5)">5</button>
         <button type="button" onclick="CheckNumber(6)">6</button>
-        <button type="button" onclick="if (number != 0) {answertext.innerText+=number} else {answertext.innerText+=(old_index+number)}">=</button>
+        <button type="button" onclick="if (New_Number) {data_calculator[current_calculator](current_number);}; answertext.innerText+=number">=</button>
     </div>
 
     <div class="customgui">
         <button type="button" onclick="CheckNumber(7)">7</button>
         <button type="button" onclick="CheckNumber(8)">8</button>
         <button type="button" onclick="CheckNumber(9)">9</button>
-        <button type="button" onclick="labeltext.innerText=''; answertext.innerText='='; number=0; old_number=0; old_index=0; current_calculator='+';">X</button>
+        <button type="button" onclick="labeltext.innerText=''; answertext.innerText='='; number=0; current_number=0; old_number=0; old_index=0; current_calculator='+';">X</button>
     </div>
 
     <p id="ShowNumber" style="border: 2px solid black; display: inline-block;"></p>
@@ -41,13 +41,15 @@
     <script>
         const labeltext = document.getElementById("ShowNumber");
         const answertext = document.getElementById("ShowAnswer");
-
+        
+        let New_Number = true;
         let old_index = 0;
         let old_number = 0;
+        let current_number = 0;
         let number = 0;
 
         const data_calculator = {
-            "+" : function(a) {number += old_number + a;},
+            "+" : function(a) {number += a;},
             "-" : function(a) {number -= a;},
             "*" : function(a) {number += old_number * a;},
             "/" : function(a) {number = old_number / a;},
@@ -56,13 +58,14 @@
 
         function CheckNumber(num) {
             if (num > -1) {
-                if (old_index > -1) {
-                    labeltext.innerText+=''
-                    old_number = (old_number*10) + num;
-                } else {
-                    data_calculator[current_calculator](num);
-                };
+                New_Number = true;
+                current_number = (current_number*10) + num;
             } else {
+                old_number = current_number;
+                current_number = 0;
+
+                New_Number = false;
+                data_calculator[current_calculator](old_number);
                 current_calculator = num;
             }
             
